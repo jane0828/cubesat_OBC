611d7124250ac2243f2a4bcd7a9cd51a6c89c922
jane <janectl29@gmail.com>
2025-06-12 13:22:49 +0900

    Update CMDACK_OBC.c

--- CMDACK_OBC.c
+++ CMDACK_OBC.c
@@ -1,37 +1,6 @@
 #include "common.h"
 #include "functions.h"
 
-// int receive_ack(int sock) {
-//     struct can_frame ack;
-//     while (1) {
-//         if (read(sock, &ack, sizeof(ack)) <= 0) continue;
-
-//         // ID 확인: FROM_CAM_ID이고, FLAG_ACK가 붙어야 함
-//         if (((ack.can_id & 0x7FF) != FROM_CAM_ID) || !(ack.can_id & FLAG_ACK)) continue;
-
-//         if (ack.can_dlc == 2) {
-//             switch (ack.data[0]) {
-//                 case CMDPIC_ID:
-//                     printf("CMDPIC 전송 성공!\n");
-//                     break;
-//                 case CMDVID_ID:
-//                     printf("CMDVID 전송 성공!\n");
-//                     break;
-//                 case CMDRESET_ID:
-//                     printf("CMDRESET 전송 성공!\n");
-//                     break;
-//                 case CMDECHO_ID:
-//                     printf("CMDECHO 전송 성공!\n");
-//                     break;
-//                 default:
-//                     printf("알 수 없는 ACK 수신: 0x%02X\n", ack.data[0]);
-//                     break;
-//             }
-//         }
-//     }
-// }
-
-
 int receive_ack(int sock) {
     struct can_frame ack;
 
@@ -70,10 +39,3 @@ int receive_ack(int sock) {
         }
     }
 }
-
-
-// void *receive_ack_thread(void *arg) {
-//     int sock = *(int *)arg;
-//     receive_ack(sock);
-//     return NULL;
-// }
